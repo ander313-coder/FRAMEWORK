c93a81ebb3805087a5c0f8a907c85ce0c6765cbb
ander313-coder <ander313@vk.com>
2024-12-03 18:24:47 +0300

    ДЗ по отображению результатов регистрации юзера
    Не могу понять как экспортировать константы на вторую страницу, на первой все корректно отображает

---
+++ UserInfo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <h1>Регистрация прошла успешно!</h1>
+        <h2>Получена информация:</h2>
+        <p>Имя: {{ user.name }}</p>
+        <p>Эл. почта: {{ user.email }}</p>
+        <p>Пороль: {{ user.password }}</p>
+        <p><RouterLink to ="/">На Главную</RouterLink></p>
+    </div>
+  </template>
+ <script lang="ts" setup>
+ import { defineComponent, ref } from 'vue'
+ import {RouterLink} from "vue-router";
+ import UserInfo from './UserReg.vue';
+ </script>
+ 
+  
+ 
---
+++ UserReg.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+      <h1>Форма обратной связи</h1>
+      <form @submit.prevent="submitForm">
+        <div>
+          <lable for="name"> Имя: </lable>
+          <input type="text" v-model="user.name" required />
+        </div>
+        <div>
+          <lable for="email"> Эл. почта: </lable>
+          <input type="email" v-model="user.email" required />
+        </div>
+            <div>
+          <lable for="password"> Пороль: </lable>
+          <input type="password" v-model="user.password" minlength="6" required />
+        </div>
+        <button type="submit">Подтвердить</button>
+      </form>
+      <div v-if="submitted">
+        <h2>Получена информация:</h2>
+        <p>Имя: {{ user.name }}</p>
+        <p>Эл. почта: {{ user.email }}</p>
+        <p>Пороль: {{ user.password }}</p>
+      </div>
+    </div>
+    <RouterLink to ="/UserInfo">На UserInfo</RouterLink>
+ 
+  </template>
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue'
+  import {RouterLink} from "vue-router";
+  export default defineComponent(
+    {
+    name: "UserInfo",
+    setup() 
+    {
+      const user = ref
+      ({
+        name: '',
+        email: '',
+        password: '',
+      });
+      const submitted = ref(false)
+      const submitForm = () => 
+      {
+        submitted.value = true;
+      };
+      return {
+        user,
+        submitted,
+        submitForm,
+      };
+    },
+  }
+  );
+  </script>
+  
